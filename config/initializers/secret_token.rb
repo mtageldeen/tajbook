d91a835503b788358505e7a6def638a2e1f996d1
Mohammed <meltahir@yahoo.com>
2013-09-24 12:47:33 +0400

    Created a rails application and added status update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tajbook::Application.config.secret_token = '94f35a3c339ed7078372eb5581d9610e94050de8daae7d1856f0aebc45114f0b235c22c2fb1270eb800d49a75050aeef0e9eccdd020b97e7b215c70e229000b8'
